335d949f72e1a4979d20b50962f005a9860deb3d
SharkBee80 <115618339+SharkBee80@users.noreply.github.com>
2026-05-28 21:57:05 +0800

    delete

--- old py/1.py
+++
@@ -1,2 +0,0 @@
-BI_RM = list("0123456789abcdefghijklmnopqrstuvwxyz")
-print(BI_RM)
--- src/ecloud.ts
+++ src/ecloud.ts
@@ -3,14 +3,10 @@ export class Ecloud {
   private readonly username: string;
   private readonly password: string;
   private client: CloudClient;
-  // private readonly UA: string;
-  // private readonly module: string;
 
   constructor(username: string, password: string, UA?: string, module?: string) {
     this.username = username;
     this.password = password;
-    // this.UA = UA || "Mozilla/5.0";
-    // this.module = module || "V2546A";
     this.client = new CloudClient({ username, password });
   }
 
